586406e6046dbcc95804f92b4351b9c3e768bc3d
ekalhxh <ekalhxh@126.com>
2024-10-07 22:13:58 +0800

    增加床面后处理程序以及相应的绘图程序

---
+++ surfData.py
@@ -0,0 +1,226 @@
+# 导入必要的库
+import os
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+from dataclasses import dataclass
+from typing import List
+from typing import Dict
+from scipy.signal import find_peaks
+from tqdm import tqdm
+
+@dataclass
+class grid_node_data:
+	x: float
+	y: float
+	z: float
+	d: float
+
+# 定义一个函数来读取任意文件
+def read_file(file_path):
+	if not os.path.exists(file_path):
+		print(f"文件 {file_path} 不存在。")
+		exit()
+
+	with open(file_path, 'r', encoding='utf-8') as file:
+		line = file.readlines()
+		return line
+
+# 读取多个文件内容, 并将其存储到一个字典中
+def read_data_file(folder_path, start_file, end_file, bed_elevation):
+	all_lines = []
+	time_step_data_dict: Dict[int, List[List[grid_node_data]]] = {}
+	for i in range(start_file, end_file+1):
+		file_path = os.path.join(folder_path, f"SurfaceData_{i}.plt")
+		results = read_file(file_path)
+		all_lines.extend(results)
+
+		for line in all_lines:
+			if "variables" in line:
+				continue
+			elif "zone" in line:
+				current_time_list = [float(num) for num in re.findall(r'\d+\.?\d*', line)]
+				current_time = round(current_time_list[0])
+			elif "i=" in line:
+				ij_range = [int(num) for num in re.findall(r'\d+', line)]
+				i_range = ij_range[0]
+				j_range = ij_range[1]
+				current_surface = [[None for _ in range(i_range)] for _ in range(j_range)]
+				current_i = 0
+				current_j = 0
+			else:
+				columns = line.split()
+				current_grid_node = grid_node_data(
+					x=float(columns[0]),
+					y=float(columns[1]),
+					z=float(columns[2]) - bed_elevation,
+					d=float(columns[3])
+				)
+				current_surface[current_j][current_i] = current_grid_node
+				current_i += 1
+				if current_i >= i_range:
+					current_i = 0
+					current_j += 1
+					if current_j >= j_range:
+						time_step_data_dict[current_time] = current_surface
+	return time_step_data_dict
+
+def auto_correlation(data1):
+	mean_data1 = np.mean(data1)
+	data2 = data1
+	corr_list = []
+	for _ in range(len(data1)):
+		mean_data12 = np.mean([data1[i] * data2[i] for i in range(len(data1))])
+		corr = mean_data12 - mean_data1 * mean_data1
+		corr_list.append(corr)
+		data2_head = data2[-1]
+		data2 = np.insert(data2[:-1], 0, data2_head)
+	return corr_list
+
+# 主程序
+if __name__ == "__main__":
+	# 定义常数
+	initial_bed = 0.05
+	nu = 1.51e-5
+	interval = 30
+	dt = 60 # 计算波速的时间间隔
+	file_interval = 240
+	user_input = False
+	if user_input:
+		# 用户输入
+		start = int(input("Please input start time (s), default=30: ") or 60)
+		end = int(input("Please input end time (s), default=1200: ") or 120)
+	else:
+		start = 30
+		end = 3600
+	# 定义文件路径
+	working_dir = "E:/Data/Sandripples1DFluid/ripple/coll"
+	cases_file = os.path.join(working_dir, "cases.dat")
+	if os.path.exists(cases_file):
+		os.remove(cases_file)
+	# 定义文件名字典
+	case_dict = {
+		0: "uStar030_250_0_2650_3600",
+		1: "uStar035_250_0_2650_3600",
+		2: "uStar040_250_0_2650_3600",
+		3: "uStar045_250_0_2650_3600",
+		4: "uStar050_250_0_2650_3600",
+		5: "uStar055_250_0_2650_3600",
+		6: "uStar060_250_0_2650_3600",
+		7: "uStar065_250_0_2650_3600",
+		#8: "uStar045_200_0_2650_3600",
+		#9: "uStar045_250_1_2000_3600",
+		#10: "uStar045_250_1_3000_3600",
+		#11: "uStar045_250_1_4000_3600",
+		#12: "uStar045_250_2_2650_3600",
+		#13: "uStar045_250_3_2650_3600",
+		#14: "uStar045_300_0_2650_3600",
+		#15: "uStar045_350_0_2650_3600",
+		#16: "uStar045_400_0_2650_3600",
+		17: "uStar040_150and350_0_2650_3600",
+	}
+	for i, folder_name in tqdm(case_dict.items(), desc="Processing", unit="case"):
+		parts = folder_name.split("_")
+		ustar_name = parts[0]
+		ustar_str = ustar_name[6:]
+		ustar_int = int(ustar_str)
+		ustar = ustar_int / 100
+		dia_name = parts[1]
+		if "and" in dia_name:
+			dia_name_list = dia_name.split("and")
+		else:
+			dia_name_list = [dia_name, dia_name]
+		dia1 = float(dia_name_list[0])/1e6
+		dia2 = float(dia_name_list[1])/1e6
+		dia =  (dia1 + dia2) / 2
+		rho_name = parts[2]
+		if rho_name == "0":
+			rho = 1.263
+		else:
+			rho = float(rho_name)
+		rhoP = float(parts[3])
+		s = rhoP/rho
+		g_hat = 9.8 * (1 - 1/s)
+		Sh = rho*ustar**2/(rhoP*g_hat*dia)
+		Ga = (s*g_hat*dia**3)**0.5/nu
+		with open(cases_file, 'a') as file:
+			file.write(f"{folder_name}\n")
+			file.write(f"u* = {ustar:.2f}, dia1={dia1:.4f}, dia2={dia2:.4f}, dia={dia:.4f}\n")
+			file.write(f"rho={rho:.4f}, rhoP={rhoP:.1f}\n")
+			file.write(f"s={s:.4f}, Sh={Sh:.4f}, Ga={Ga:.4f}\n")
+			file.write("\n")
+
+		last_time = int(parts[4])
+		real_end = min(end, last_time)
+		start_file_num = start // file_interval
+		end_file_num = real_end // file_interval
+		folder_path = f"{working_dir}/{folder_name}/Surface"
+		time_step_data = read_data_file(folder_path, start_file_num, end_file_num, initial_bed)
+		proflie_t = []
+		surface_profile_file = os.path.join(working_dir, f"{folder_name}/surfProfile.dat")
+		auto_corr_file = os.path.join(working_dir, f"{folder_name}/autoCorr.dat")
+		surface_topology_file = os.path.join(working_dir, f"{folder_name}/surfTopology.dat")
+		if os.path.exists(surface_profile_file):
+			os.remove(surface_profile_file)
+		if os.path.exists(auto_corr_file):
+			os.remove(auto_corr_file)
+		if os.path.exists(surface_topology_file):
+			os.remove(surface_topology_file)
+		wavelength_t = []
+		amplitude_t = []
+		migration_t = []
+		time = []
+		time_step_profile: Dict[int, List[float]] = {}
+		lag = 0
+		for t, surface in time_step_data.items():
+			if t>=start and t<=end:
+				profile_x = np.array([node.x for node in surface[0]])
+				surface_z = np.array([[node.z for node in row] for row in surface])
+				profile_z = np.mean(surface_z, axis=0)
+				time_step_profile[t] = profile_z
+				with open(surface_profile_file, 'a') as file:
+					file.write(f"x vs z at {t} with {len(profile_x)} points\n")
+					for i in range(len(profile_x)):
+						file.write(f"{profile_x[i]} {profile_z[i]}\n")
+				auto_corr_x = profile_x
+				auto_corr = np.correlate(profile_z, profile_z, mode='full')
+				auto_corr_z = auto_corr[len(auto_corr)//2:]
+				#auto_corr_z = auto_correlation(profile_z)
+				with open(auto_corr_file, 'a') as file:
+					file.write(f"x vs corr at {t} with {len(auto_corr_x)} points\n")
+					for i in range(len(auto_corr_x)):
+						file.write(f"{auto_corr_x[i]} {auto_corr_z[i]}\n")
+				peaks, _ = find_peaks(auto_corr_z, height=0)
+				if len(peaks) > 0:
+					lag = peaks[0]
+				displacement = auto_corr_x[lag]
+				amplitude = 2*np.sqrt(2*auto_corr_z[0])
+				time.append(t)
+				wavelength_t.append(displacement)
+				amplitude_t.append(amplitude)
+		times = list(time_step_profile.keys())
+		velocities = []
+		di = dt // interval
+		lag = 0
+		for i in range(len(times)-di):
+			t1 = times[i]
+			t2 = times[i+di]
+			z1 = time_step_profile[t1]
+			z2 = time_step_profile[t2]
+			cross_corr = np.correlate(z1, z2, mode='full')
+			cross_start = len(z1) - 1
+			cross_corr_z = cross_corr[cross_start:]
+			peaks, _ = find_peaks(cross_corr_z, height=0)
+			if len(peaks) > 0:
+				lag = peaks[0]
+			displacement = profile_x[lag]
+			time_diff = t2 - t1
+			velocity = displacement / time_diff
+			velocities.append(velocity)
+		# Ensure velocities has the same length as times by appending the last element
+		while len(velocities) < len(times):
+			velocities.append(velocities[-1])
+		with open(surface_topology_file, 'w') as file:
+			file.write(f"lambda amplitude velocity vs t with {len(time)} points\n")
+			for i in range(len(time)):
+				file.write(f"{time[i]} {wavelength_t[i]} {amplitude_t[i]} {velocities[i]}\n")
---
+++ surfPlot.py
@@ -0,0 +1,223 @@
+# 导入必要的库
+import math
+import os
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+from dataclasses import dataclass
+from typing import List
+from typing import Dict
+from scipy.signal import find_peaks
+
+@dataclass
+class file_data:
+	data: List[float]
+
+# 定义一个函数来读取任意文件
+def read_file(file_path):
+	if not os.path.exists(file_path):
+		print(f"文件 {file_path} 不存在。")
+		exit()
+
+	with open(file_path, 'r', encoding='utf-8') as file:
+		line = file.readlines()
+		return line
+
+# 读取文件内容, 并将其存储到一个字典中
+def read_data_file1(folder_path, file_name):
+	file_path = os.path.join(folder_path, file_name)
+	all_lines = read_file(file_path)
+
+	time_step_data_dict: Dict[int, List[file_data]] = {}
+	for line in all_lines:
+		if "vs" in line:
+			nums_in_first_line = [int(num) for num in re.findall(r'\d+\.?\d*', line)]
+			current_time = nums_in_first_line[0]
+			point_num = nums_in_first_line[1]
+			data_list = []
+			point_count = 0
+		else:
+			point_count += 1
+			columns = line.split()
+			columns_float = [float(column) for column in columns]
+			current_data = file_data(
+				data=columns_float
+			)
+			data_list.append(current_data)
+			if point_count == point_num:
+				time_step_data_dict[current_time] = data_list
+	return time_step_data_dict
+
+def read_data_file2(folder_path, file_name):
+	file_path = os.path.join(folder_path, file_name)
+	all_lines = read_file(file_path)
+
+	data_list = []
+	for line in all_lines:
+		if "vs" not in line:
+			columns = line.split()
+			columns_float = [float(column) for column in columns]
+			current_data = file_data(
+				data=columns_float
+			)
+			data_list.append(current_data)
+	return data_list
+
+# 主程序
+if __name__ == "__main__":
+	# 固定参数
+	nu = 1.51e-5
+	interval = 30
+	# 控制参数
+	output_num = 4 # 出图类型：0为廓线，1为相关性，2为波长, 3为波高, 4为波速
+	start = 30 # 起始时间
+	end = 2400 # 结束时间
+	average_start = 30 # 开始计算平均值的时间
+	average_end = 900 # 结束计算平均值的时间
+	profile_offset = 2e-4 # 廓线图的纵向偏移
+	corr_offset = 1e-8 # 相关性图的纵向偏移
+	case_num = 3 # 算例号
+	case_num_dict = {
+		0: 0,
+		1: 1,
+		2: 2,
+		3: 3,
+		4: 4,
+		5: 5,
+		6: 6,
+		7: 7,
+		#8: 17,
+	}
+	# 定义文件路径
+	working_dir = "E:/Data/Sandripples1DFluid/ripple/coll"
+	# 定义文件名字典
+	case_dict = {
+		0: "uStar030_250_0_2650_3600",
+		1: "uStar035_250_0_2650_3600",
+		2: "uStar040_250_0_2650_3600",
+		3: "uStar045_250_0_2650_3600",
+		4: "uStar050_250_0_2650_3600",
+		5: "uStar055_250_0_2650_3600",
+		6: "uStar060_250_0_2650_3600",
+		7: "uStar065_250_0_2650_3600",
+		8: "uStar045_200_0_2650_3600",
+		9: "uStar045_250_1_2000_3600",
+		10: "uStar045_250_1_3000_3600",
+		11: "uStar045_250_1_4000_3600",
+		12: "uStar045_250_2_2650_3600",
+		13: "uStar045_250_3_2650_3600",
+		14: "uStar045_300_0_2650_3600",
+		15: "uStar045_350_0_2650_3600",
+		16: "uStar045_400_0_2650_3600",
+		17: "uStar040_150and350_0_2650_3600",
+	}
+	output_file_dict = {
+		0: "surfProfile.dat",
+		1: "autoCorr.dat",
+		2: "surfTopology.dat",
+		3: "surfTopology.dat",
+		4: "surfTopology.dat",
+	}
+
+	output_file = output_file_dict[output_num]
+	if output_num == 0 or output_num == 1:
+		folder_name = case_dict[case_num]
+		folder_path = f"{working_dir}/{folder_name}"
+		all_plot_data = read_data_file1(folder_path, output_file)
+		offset = 0
+		# 设置图像的宽度和高度
+		plt.figure(figsize=(10, 3))  # 宽度为10，高度为3
+		for t in range(start, end+1, interval):
+			current_plot_data = all_plot_data[t]
+			x = [point.data[0] for point in current_plot_data]
+			y = [point.data[1]+offset for point in current_plot_data]
+			if output_num == 0:
+				offset += profile_offset
+			elif output_num == 1:
+				offset += corr_offset
+			plt.plot(x, y, color='black', linewidth=0.5)  # 设置线的颜色为黑色，线的粗细为0.5
+
+		plt.xlabel('x')
+		plt.ylabel('t')
+		plt.gca().yaxis.set_ticks([])  # 移除纵坐标刻度
+		plt.xlim(min(x), max(x))  # 设置横坐标范围为数据的最小值和最大值
+		plt.show()
+	elif output_num >= 2 and output_num <= 4:
+		y_list = []
+		y_max = 0
+		y_min = 1000
+		for i, case in case_num_dict.items():
+			folder_name = case_dict[case]
+			folder_path = f"{working_dir}/{folder_name}"
+			parts = folder_name.split("_")
+			ustar_name = parts[0]
+			ustar_str = ustar_name[6:]
+			ustar_int = int(ustar_str)
+			ustar = ustar_int / 100
+			dia_name = parts[1]
+			if "and" in dia_name:
+				dia_name_list = dia_name.split("and")
+			else:
+				dia_name_list = [dia_name, dia_name]
+			dia1 = float(dia_name_list[0])/1e6
+			dia2 = float(dia_name_list[1])/1e6
+			dia =  (dia1 + dia2) / 2
+			rho_name = parts[2]
+			if rho_name == "0":
+				rho = 1.263
+			else:
+				rho = float(rho_name)
+			rhoP = float(parts[3])
+			s = rhoP/rho
+			g_hat = 9.8 * (1 - 1/s)
+			Sh = rho*ustar**2/(rhoP*g_hat*dia)
+			Ga = (s*g_hat*dia**3)**0.5/nu
+			all_plot_data = read_data_file2(folder_path, output_file)
+			x = [point.data[0] for point in all_plot_data]
+			if output_num == 2:
+				y = [point.data[1] for point in all_plot_data]
+			elif output_num == 3:
+				y = [point.data[2] for point in all_plot_data]
+			elif output_num == 4:
+				y = [point.data[3] for point in all_plot_data]
+			i_start = start // interval - 1
+			i_end = end // interval - 1
+			y_max = max(y_max, max(y[i_start:i_end]))
+			y_min = min(y_min, min(y[i_start:i_end]))
+			# Apply 3-point box filter to y
+			y_filtered = np.convolve(y, np.ones(3)/3, mode='same')
+			y = y_filtered
+			i_start = average_start // interval - 1
+			i_end = average_end // interval - 1
+			average_y = np.mean(y[i_start:i_end])
+			y_list.append((ustar, dia1, dia2, dia, Sh, Ga, average_y))
+			plt.figure(1)
+			if dia1 == dia2:
+				plt.plot(x, y, label=f"$u*={ustar}$, $d={dia}$")
+			else:
+				plt.plot(x, y, label=f"$u*={ustar}$, $d_1={dia1}$, $d_2={dia2}$")
+		plt.xlabel('$t$')
+		if output_num == 2:
+			plt.ylabel('$\lambda$')
+			plt.ylim(0, y_max)
+		elif output_num == 3:
+			plt.yscale('log')
+			plt.ylabel('$A$')
+			plt.ylim(y_min, y_max)
+		elif output_num == 4:
+			plt.ylabel('$c$')
+			plt.ylim(0, y_max)
+		plt.legend(loc='upper left')
+		plt.xlim(start, end)
+		plt.show()
+
+		plt.figure(2)
+		plt.plot([item[0] for item in y_list], [item[6] for item in y_list], 'o')
+		plt.xlabel('$u*$')
+		if output_num == 2:
+			plt.ylabel('$\lambda$')
+		elif output_num == 3:
+			plt.ylabel('$A$')
+		elif output_num == 4:
+			plt.ylabel('$c$')
+		plt.show()
